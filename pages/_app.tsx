d1c2e44a4aa270a60333d77b5a2c401a9f899d7f
Will Naugle <willynogs@gmail.com>
2023-02-24 12:53:58 -0800

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import Footer from '@/components/Footer'
 import { ChakraProvider, Container, Grid, GridItem } from '@chakra-ui/react'
 import theme from '@/theme'
 
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
@@ -28,6 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
           </GridItem>
         </Grid>
       </Container>
+      <Analytics />
     </ChakraProvider>
   )
 }
